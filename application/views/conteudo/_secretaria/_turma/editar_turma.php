f2443e1eca1f33fdf6a9382fc22eafc6209f449a
Ermano Cristovão <ercris990@gmail.com>
2020-11-15 00:26:42 +0100

    turmas

--- application/views/conteudo/_secretaria/_turma/editar_turma.php
+++ application/views/conteudo/_secretaria/_turma/editar_turma.php
@@ -1,69 +1,68 @@
 <!-- INICIO CONTEUDO -->
 <div id="content" class="content">
-    <!-- begin page-header -->
-    <h4 class="page-header"><i class="fa fa-list mr-5"></i>EDITAR TURMA</h4>
-    <?= $this->session->flashdata('msg'); ?>
-    <!-- begin table-responsive -->
-    <div class="col-6 mx-auto my-5">
-        <form action="<?php echo site_url('secretaria/turma/salvaractualizacao')?>" method="POST">
-            <input type="hidden" name="id_turma" id="id_turma" value="<?= $turma[0]->id_turma; ?>">
-                <!-- CAMPO NOME TURMA -->
-                <div class="form-group col-12">
-                    <label for="nome_turma">Nome da turma</label>
-                    <input type="text" name="nome_turma" class="form-control border-primary text-primary"
-                        id="nome_turma" minlength="2" placeholder="1A-M" autocomplete="off"
-                        value=" <?= $turma[0]->nome_turma; ?>" />
-                </div>
-                <!-- CAMPO CLASSE -->
-                <div class="form-group col-12">
-                    <label for="nome_classe">Classe</label>
-                    <select name="nome_classe" class="form-control border-primary text-primary" id="nome_classe">
-                        <!------------------------------------------------------------>
-                        <?php foreach($classe as $cls):       
+  <!-- begin page-header -->
+  <h4 class="page-header"><i class="fa fa-list mr-5"></i>EDITAR TURMA</h4>
+  <?= $this->session->flashdata('msg'); ?>
+  <!-- begin table-responsive -->
+  <div class="col-6 mx-auto my-5">
+    <form action="<?php echo site_url('secretaria/turma/salvaractualizacao')?>" method="POST">
+      <input type="hidden" name="id_turma" id="id_turma" value="<?= $turma[0]->id_turma; ?>">
+      <!-- CAMPO NOME TURMA -->
+      <div class="form-group col-12">
+        <label for="nome_turma">Nome da turma</label>
+        <input type="text" name="nome_turma" class="form-control border-primary text-primary" id="nome_turma"
+          minlength="2" placeholder="1A-M" autocomplete="off" value=" <?= $turma[0]->nome_turma; ?>" />
+      </div>
+      <!-- CAMPO CLASSE -->
+      <div class="form-group col-12">
+        <label for="nome_classe">Classe</label>
+        <select name="nome_classe" class="form-control border-primary text-primary" id="nome_classe">
+          <!------------------------------------------------------------>
+          <?php foreach($classe as $cls):       
                                         if($turma[0]->classe_id==$cls->id_classe) { ?>
-                        <option value="<?= $cls->id_classe ?>" selected=""> <?= $cls->nome_classe; ?>
-                        </option>
-                        <?php 
+          <option value="<?= $cls->id_classe ?>" selected=""> <?= $cls->nome_classe; ?>
+          </option>
+          <?php 
                                         } else{ ?>
-                        <option value="<?= $cls->id_classe ?>"> <?= $cls->nome_classe; ?> </option><?php 
+          <option value="<?= $cls->id_classe ?>"> <?= $cls->nome_classe; ?> </option><?php 
                                         } 
                                 endforeach; ?>
-                        <!------------------------------------------------------------>
-                    </select>
-                </div>
-                <!-- CAMPO SALA -->
-                <div class="form-group col-12">
-                    <label for="numero_sala">Sala</label>
-                    <select name="numero_sala" class="form-control border-primary text-primary">
-                        <!---------------------------->
-                        <?php foreach($sala as $sl):       
+          <!------------------------------------------------------------>
+        </select>
+      </div>
+      <!-- CAMPO SALA -->
+      <div class="form-group col-12">
+        <label for="numero_sala">Sala</label>
+        <select name="numero_sala" class="form-control border-primary text-primary">
+          <!---------------------------->
+          <?php foreach($sala as $sl):       
                                         if($turma[0]->sala_id==$sl->id_sala) { ?>
-                        <option value="<?= $sl->id_sala ?>" selected=""> <?= $sl->numero_sala; ?>
-                        </option><?php 
+          <option value="<?= $sl->id_sala ?>" selected=""> <?= $sl->numero_sala; ?>
+          </option><?php 
                                         } else{ ?>
-                        <option value="<?= $sl->id_sala ?>"> <?= $sl->numero_sala; ?> </option><?php 
+          <option value="<?= $sl->id_sala ?>"> <?= $sl->numero_sala; ?> </option><?php 
                                         } 
                                 endforeach; ?>
-                    </select>
-                </div>
-                <!-- CAMPO PERIODO -->
-                <div class="form-group col-12">
-                    <label for="nome_periodo">Período</label>
-                    <select name="nome_periodo" class="form-control border-primary text-primary" id="nome_periodo">
-                        <!------------------------------------------------------------>
-                        <?php foreach($periodo as $prd):       
+        </select>
+      </div>
+      <!-- CAMPO PERIODO -->
+      <div class="form-group col-12">
+        <label for="nome_periodo">Período</label>
+        <select name="nome_periodo" class="form-control border-primary text-primary" id="nome_periodo">
+          <!------------------------------------------------------------>
+          <?php foreach($periodo as $prd):       
                                         if($turma[0]->periodo_id==$prd->id_periodo) { ?>
-                        <option value="<?= $prd->id_periodo ?>" selected=""> <?= $prd->nome_periodo; ?>
-                        </option><?php 
+          <option value="<?= $prd->id_periodo ?>" selected=""> <?= $prd->nome_periodo; ?>
+          </option><?php 
                                         } else{ ?>
-                        <option value="<?= $prd->id_periodo ?>"> <?= $prd->nome_periodo; ?> </option><?php 
+          <option value="<?= $prd->id_periodo ?>"> <?= $prd->nome_periodo; ?> </option><?php 
                                         } 
                                 endforeach; ?>
-                        <!------------------------------------------------------------>
-                    </select>
-                </div>
-                <div class="form-group col-12">
-                    <button type="submit" class="btn btn-primary btn-block mt-3"><i class="fa fa-edit mr-2"></i>ACTUALIZAR</button>
-                </div>
-        </form>
-    </div><!-- end col-5 -->
+          <!------------------------------------------------------------>
+        </select>
+      </div>
+      <div class="form-group col-12">
+        <button type="submit" class="btn btn-primary btn-block mt-3"><i class="fa fa-edit mr-2"></i>ACTUALIZAR</button>
+      </div>
+    </form>
+  </div><!-- end col-5 -->
--- application/views/conteudo/_secretaria/_turma/nova_turma.php
+++ application/views/conteudo/_secretaria/_turma/nova_turma.php
@@ -1,52 +1,52 @@
 <!-- INICIO CONTEUDO -->
 <div id="content" class="content">
-    <!-- begin page-header -->
-    <h4 class="page-header"><i class="fa fa-list mr-5"></i>NOVA TURMA</h4>
-    <?= $this->session->flashdata('msg'); ?>
-    <!-- begin table-responsive -->
-    <div class="col-6 mx-auto my-5">
-        <form action="<?php echo site_url('secretaria/turma/guardar')?>" method="POST" id="form_turma">
-            <fieldset>
-                    <div class="form-group col-12">
-                        <label for="nome_turma">Nome da Turma</label>
-                        <input type="text" name="nome_turma" id="nome_turma"
-                            class="form-control border-primary text-primary" placeholder="1A-M" autocomplete="off" />
-                    </div>
-                    <!--------------------------- SELECT DINAMICO ---------------------------------------->
-                    <div class="form-group col-12">
-                        <label for="nome_classe">Classe</label>
-                        <select name="nome_classe" class="form-control border-primary text-primary">
-                            <option value="">Selecione a classe</option>
-                            <?php foreach($classe as $cls): ?>
-                            <option value="<?= $cls->id_classe ?>"> <?= $cls->nome_classe; ?> </option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <!--------------------------- SELECT DINAMICO ---------------------------------------->
-                    <div class="form-group col-12">
-                        <label for="numero_sala">Sala</label>
-                        <select name="numero_sala" class="form-control border-primary text-primary">
-                            <option value="">Selecione a sala</option>
-                            <!---------------------------->
-                            <?php foreach($sala as $sl): ?>
-                            <option value="<?= $sl->id_sala ?>"> <?= $sl->numero_sala; ?> </option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <!--------------------------- SELECT DINAMICO ---------------------------------------->
-                    <div class="form-group col-12">
-                        <label for="nome_periodo">Periodo</label>
-                        <select name="nome_periodo" class="form-control border-primary text-primary">
-                            <option value="">Selecione o periodo</option>
-                            <!---------------------------->
-                            <?php foreach($periodo as $prd): ?>
-                            <option value="<?= $prd->id_periodo ?>"> <?= $prd->nome_periodo; ?> </option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
-                    <div class="form-group col-12">
-                        <button type="submit" class="btn btn-primary btn-block mt-3"><i class="fa fa-save mr-2"></i>GUARDAR</button>
-                    </div>
-            </fieldset>
-        </form>
-    </div><!-- end col-5 -->
+  <!-- begin page-header -->
+  <h4 class="page-header"><i class="fa fa-list mr-5"></i>NOVA TURMA</h4>
+  <?= $this->session->flashdata('msg'); ?>
+  <!-- begin table-responsive -->
+  <div class="col-6 mx-auto my-5">
+    <form action="<?php echo site_url('secretaria/turma/guardar')?>" method="POST" id="form_turma">
+      <fieldset>
+        <div class="form-group col-12">
+          <label for="nome_turma">Nome da Turma</label>
+          <input type="text" name="nome_turma" id="nome_turma" class="form-control border-primary text-primary"
+            placeholder="1A-M" autocomplete="off" />
+        </div>
+        <!--------------------------- SELECT DINAMICO ---------------------------------------->
+        <div class="form-group col-12">
+          <label for="nome_classe">Classe</label>
+          <select name="nome_classe" class="form-control border-primary text-primary">
+            <option value="">Selecione a classe</option>
+            <?php foreach($classe as $cls): ?>
+            <option value="<?= $cls->id_classe ?>"> <?= $cls->nome_classe; ?> </option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+        <!--------------------------- SELECT DINAMICO ---------------------------------------->
+        <div class="form-group col-12">
+          <label for="numero_sala">Sala</label>
+          <select name="numero_sala" class="form-control border-primary text-primary">
+            <option value="">Selecione a sala</option>
+            <!---------------------------->
+            <?php foreach($sala as $sl): ?>
+            <option value="<?= $sl->id_sala ?>"> <?= $sl->numero_sala; ?> </option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+        <!--------------------------- SELECT DINAMICO ---------------------------------------->
+        <div class="form-group col-12">
+          <label for="nome_periodo">Periodo</label>
+          <select name="nome_periodo" class="form-control border-primary text-primary">
+            <option value="">Selecione o periodo</option>
+            <!---------------------------->
+            <?php foreach($periodo as $prd): ?>
+            <option value="<?= $prd->id_periodo ?>"> <?= $prd->nome_periodo; ?> </option>
+            <?php endforeach; ?>
+          </select>
+        </div>
+        <div class="form-group col-12">
+          <button type="submit" class="btn btn-primary btn-block mt-3"><i class="fa fa-save mr-2"></i>GUARDAR</button>
+        </div>
+      </fieldset>
+    </form>
+  </div><!-- end col-5 -->
